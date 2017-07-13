77b0dd8b6c6c449bafb6a11950df29d13d14d6b9
mrobertsASC4 <slashgray147@gmail.com>
2017-07-13 12:02:30 -0700

    added a new file

---
+++ pokemon-roster.js
@@ -0,0 +1,14 @@
+var pokemonRoster = [
+    ["Bulbasur" , "Grass" , 45 , 49 , 49 , false]
+    ["Pidgeot" , "Flying/Normal" , 83 , 75 , 80 , false]
+    ["Ninetales" , "Fire" , 73 , 75 , 76 , false]
+    ["Gyarados" , "Water/Flying" , 95 , 79 , 125 , false]
+    ["Aggron" , "Steel/Rock" , 70 , 180 , 110 , false]
+    ["Regice", "Ice", 80 , 100 , 50 , true]
+    ]
+
+function loop() {
+    while (var i = 0)
+    i < pokemonRoster.length; i++)
+    return()
+}
