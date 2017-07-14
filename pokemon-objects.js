99c43a21954ded9440bed649d0251a03a3f46768
mrobertsASC4 <slashgray147@gmail.com>
2017-07-14 15:43:10 -0700

    updated previous files and added a few more

---
+++ pokemon-objects.js
@@ -0,0 +1,36 @@
+var pokemonRoster = [
+    {"name" : "Bulbasur" , "type" : "Grass" , "hp" : 45 , "def" : 49 , "atk" : 49 , "legendary" : false } ,
+    {"name" : "Pidgeot" , "type" : "Flying/Normal" , "hp" : 83 , "def" : 75 , "atk" : 80 , "legendary" : false },
+    {"name" : "Ninetales" , "type" : "Fire" , "hp" : 73 , "def" : 75 ,"atk" : 76 , "legendary" : false } ,
+    {"name" : "Gyarados" , "type" : "Water/Flying" , "hp" : 95 , "def" : 79 , "atk" : 125 , "legendary" : false } ,
+    {"name" : "Aggron" , "type" : "Steel/Rock" , "hp" : 70 , "def" : 180 , "atk" : 110 , "legendary" : false } ,
+    {"name" : "Regice" , "type" : "Ice" , "hp" : 80 , "def" : 100 , "atk" : 50 , "legendary" : true }
+    ]
+    //Prints Roster//
+    function printRoster() {
+        for (var i = 0 ; i < pokemonRoster.length ; i++) {
+            console.log(pokemonRoster[i])
+    } 
+    }
+    
+    printRoster();
+    
+    //Attacked Pokemons/
+    function pokemonAttacked() {
+        for (var i = 0 ; i < pokemonRoster.length ; i++) {
+            pokemonRoster[i].hp = pokemonRoster[i].hp - 10
+
+            
+        }
+    }
+    
+    pokemonAttacked();
+    printRoster();
+
+function subtractHP() {
+    var prompt = require('prompt-sync')();
+    var minusHP = prompt("How much HP would you like to subtract? ");
+    console.log("You took away " + minusHP + " from " + )
+ }
+
+ subtractHP();
--- pokemon-roster.js
+++ pokemon-roster.js
@@ -1,19 +1,21 @@
 var pokemonRoster = [
-    ["Bulbasur" , "Grass" , 45 , 49 , 49 , false ] 
-    ["Pidgeot" , "Flying/Normal" , 83 , 75 , 80 , false ]
-    ["Ninetales" , "Fire" , 73 , 75 , 76 , false ]
-    ["Gyarados" , "Water/Flying" , 95 , 79 , 125 , false ]
-    ["Aggron" , "Steel/Rock" , 70 , 180 , 110 , false ]
-    ["Regice", "Ice", 80 , 100 , 50 , true ]
+    ["Bulbasur" , "Grass" , 45 , 49 , 49 , false ] , 
+    ["Pidgeot" , "Flying/Normal" , 83 , 75 , 80 , false ] ,
+    ["Ninetales" , "Fire" , 73 , 75 , 76 , false ] ,
+    ["Gyarados" , "Water/Flying" , 95 , 79 , 125 , false ] ,
+    ["Aggron" , "Steel/Rock" , 70 , 180 , 110 , false ] ,
+    ["Regice", "Ice", 80 , 100 , 50 , true ] ,
     ]
 
 function loop() {
-    for (var i = 0; i < pokemonRoster.length; i++)
-    console.log("Name: " + p[i][0]);
-    console.log("Type: " + p[i][1]);
-    console.log("HP: " + p[i][2]);
-    console.log("Attack: " + p[i][3]);
-    console.log("Defense: " + p[i][4]);
-    console.log("Legend: " + p[i][5])
-
+    for (var i = 0; i < pokemonRoster.length; i++) {
+        console.log("Name: " + pokemonRoster[i][0]);
+        console.log("Type: " + pokemonRoster[i][1]);
+        console.log("HP: " + pokemonRoster[i][2]);
+        console.log("Attack: " + pokemonRoster[i][3]);
+        console.log("Defense: " + pokemonRoster[i][4]);
+        console.log("Legend: " + pokemonRoster[i][5])
+    }
 }
+ 
+ loop();
